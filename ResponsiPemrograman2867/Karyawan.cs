2f247bd5755dfb477d0c1337f994aff958240e16
bagas <bagas.nugroho@students.amikom.ac.id>
2020-04-15 15:24:06 +0700

    Responsi

---
+++ ResponsiPemrograman2867/Karyawan.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Reflection.Metadata.Ecma335;
+
+namespace ResponsiPemrograman2867
+{
+    public class Karyawan
+    {
+        public int No { get; set; }
+        public int NIK { get; set; }
+        public string Nama { get; set; }
+        public int Gaji { get; set; }
+
+        public Karyawan(int no, int nik, string nama, int gaji)
+        {
+            Nama = Nama;
+            NIK = nik;
+            Gaji = gaji;
+            No = no;
+        }
+
+        public void gaji()
+        {
+            Console.WriteLine("{0}no : {1} nik  : {2} gaji {3}", No, NIK, Nama, Gaji);
+        }
+        public void gajinaik()
+        {
+  
+            Console.WriteLine("Karyawan has nama : {0} no : {1} nik : (2) gaji (3)", No, NIK, Nama, Gaji);
+        }
+
+    }
+}
---
+++ ResponsiPemrograman2867/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace ResponsiPemrograman2867
+{
+    public class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Karyawan karyawan = new Karyawan( 1 , 19112000, "dai", 500000);
+            karyawan.gaji();
+
+            Karyawan karyawan2 = new Karyawan(2, 19112001, "udin", 500000);
+            karyawan2.gaji();
+
+          
+        }
+    }
+}
+
+        
